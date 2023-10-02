aae16f7583958c874ac9b24d0857a930666c10c0
moh1med <mohamedch047@gmail.com>
2023-10-02 14:42:36 +0100

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+function handelPass(){
+    console.log('Entering password...');
+    }
+    function Keypad (){
+        return (
+            <div>
+    <input type="password" name="password" placeholder="put ur password" onChange={handelPass}/>
+    
+            </div>
+        )
+        
+    }
+    
+    export default Keypad;
